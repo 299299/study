fd45798454704887ad767b0d073848fc7a1bd794
goldenli <Golden.Li@bmw.com>
2019-01-18 09:00:21 +0800

    grokking algorithms

---
+++ grokking algorithms/code/code_answers/07_dijkstras_algorithm/python/01_dijkstras_algorithm.py
@@ -0,0 +1,85 @@
+# the graph
+graph = {}
+graph["start"] = {}
+graph["start"]["a"] = 3
+graph["start"]["b"] = 2
+
+graph["a"] = {}
+graph["a"]["fin"] = 1
+
+graph["b"] = {}
+graph["b"]["a"] = 2
+graph["b"]["fin"] = 5
+
+graph["fin"] = {}
+
+# the costs table
+infinity = float("inf")
+costs = {}
+costs["a"] = 3
+costs["b"] = 2
+costs["fin"] = infinity
+
+# the parents table
+parents = {}
+parents["a"] = "start"
+parents["b"] = "start"
+parents["fin"] = None
+
+processed = []
+
+def find_lowest_cost_node(costs):
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    # Go through each node.
+    for node in costs:
+        cost = costs[node]
+        # If it's the lowest cost so far and hasn't been processed yet...
+        if cost < lowest_cost and node not in processed:
+            # ... set it as the new lowest-cost node.
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+# Find the lowest-cost node that you haven't processed yet.
+node = find_lowest_cost_node(costs)
+
+print ' 1 node={}'.format(node)
+
+# If you've processed all the nodes, this while loop is done.
+while node is not None:
+
+    print '---------------------------------'
+    print 'node={}'.format(node)
+    print 'costs={}'.format(costs)
+    print ('parents={}'.format(parents))
+
+    cost = costs[node]
+    # Go through all the neighbors of this node.
+    neighbors = graph[node]
+
+    print 'cost={}'.format(cost)
+    print 'neighbors={}'.format(neighbors)
+
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+
+        print 'n={}, new_cost={}'.format(n, new_cost)
+
+        # If it's cheaper to get to this neighbor by going through this node...
+        if costs[n] > new_cost:
+            # ... update the cost for this node.
+            costs[n] = new_cost
+            # This node becomes the new parent for this neighbor.
+            parents[n] = node
+    # Mark the node as processed.
+    processed.append(node)
+    # Find the next node to process, and loop.
+    node = find_lowest_cost_node(costs)
+
+    print ('22 costs={}'.format(costs))
+    print ('22 parents={}'.format(parents))
+
+print("Cost from the start to each node:")
+print(costs)
+
